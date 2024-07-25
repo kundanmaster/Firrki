1ac0b9b267b247fd68d9732d3972df9e357e88c5
kundanmaster <kundan.masterbazar@gmail.com>
2024-07-26 04:13:35 +0530

    changes in api routes

--- src/app/(pages)/api/signup/route.ts
+++ src/app/(pages)/api/signup/route.ts
@@ -1,10 +1,8 @@
-import { NextResponse } from 'next/server';
-// import  pool  from '../../../../lib/db';
+import { NextResponse, NextRequest } from 'next/server';
 import bcrypt from 'bcrypt';
 import client from '../../../../lib/db';
 
-export const POST = async (request: { json: () => any; }) => {
-
+export const POST = async (request: NextRequest) => {
   const body = await request.json();
 
   const { name, email, password } = body;
@@ -13,26 +11,17 @@ export const POST = async (request: { json: () => any; }) => {
     return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
   }
 
-//   const client = await pool.connect();
-
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    // const role ="user";
     const result = await client.query(
       'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id',
       [name, email, hashedPassword]
     );
 
     return NextResponse.json({ success: true, userId: result.rows[0].id }, { status: 201 });
-
   } catch (error) {
     console.error('Error during signup:', error);
 
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
-
-  } finally {
-
-    // client.release();
-
   }
 };
