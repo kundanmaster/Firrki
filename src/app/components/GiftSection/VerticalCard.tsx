1e85d897960826cdebadf5a2f73f2f53088b5030
Ankit <ankit@stylopay.com>
2024-09-08 05:58:20 +0530

    updated design

--- src/app/components/GiftSection/VerticalCard.tsx
+++ src/app/components/GiftSection/VerticalCard.tsx
@@ -1,129 +1,13 @@
-// "use client";
-// import Image from "next/image";
-// import React, { useState } from "react";
-
-// const images = [
-//   "/assets/tableLinen/image6.png",
-//   "/assets/tableLinen/image8.png",
-//   "/assets/tableLinen/image6.png",
-//   "/assets/tableLinen/image8.png",
-// ];
-
-// const infoSections = [
-//   {
-//     title: "Lorem ipsum",
-//     subtitle: "Lorem ipsum odor amet,",
-//     description:
-//       "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh enim finibus dignissim montes condimentum imperdiet eget? Torquent tortor dolor bibendum dui eu purus. Etiam elit feugiat nisl penatibus bibendum nam. Congue lobortis suspendisse rutrum vestibulum eu fusce class. Orci habitant commodo magna consectetur ultrices vestibulum dolor per.",
-//   },
-//   {
-//     title: "Lorem ipsum 2",
-//     subtitle: "Lorem ipsum odor amet, 2",
-//     description:
-//       "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh enim finibus dignissim montes condimentum imperdiet eget? Torquent tortor dolor bibendum dui eu purus. Etiam elit feugiat nisl penatibus bibendum nam. Congue lobortis suspendisse rutrum vestibulum eu fusce class. Orci habitant commodo magna consectetur ultrices vestibulum dolor per.",
-//   },
-//   {
-//     title: "Lorem ipsum 3",
-//     subtitle: "Lorem ipsum odor amet, 3",
-//     description:
-//       "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh enim finibus dignissim montes condimentum imperdiet eget? Torquent tortor dolor bibendum dui eu purus. Etiam elit feugiat nisl penatibus bibendum nam. Congue lobortis suspendisse rutrum vestibulum eu fusce class. Orci habitant commodo magna consectetur ultrices vestibulum dolor per.",
-//   },
-//   {
-//     title: "Lorem ipsum 4",
-//     subtitle: "Lorem ipsum odor amet, 4",
-//     description:
-//       "Lorem ipsum odor amet, consectetuer adipiscing elit. Nibh enim finibus dignissim montes condimentum imperdiet eget? Torquent tortor dolor bibendum dui eu purus. Etiam elit feugiat nisl penatibus bibendum nam. Congue lobortis suspendisse rutrum vestibulum eu fusce class. Orci habitant commodo magna consectetur ultrices vestibulum dolor per.",
-//   },
-// ];
-
-// const VerticalCard = () => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   const handleDotClick = (index: React.SetStateAction<number>) => {
-//     setCurrentIndex(index);
-//   };
-
-//   return (
-//     <>
-//       <div className="py-2">
-//         <div className="relative flex h-[100vh] w-full bg-white shadow-lg overflow-hidden">
-//           {/* Image Slider Section */}
-//           <div className="flex-none w-[50%] h-full overflow-hidden relative">
-//             {images.map((src, index) => (
-//               <div
-//                 key={index}
-//                 className={`absolute inset-0 transition-all duration-500 ease-in-out transform ${
-//                   index === currentIndex
-//                     ? "opacity-100 translate-y-0 z-10"
-//                     : "opacity-0 -translate-y-full z-0"
-//                 }`}
-//               >
-//                 <Image
-//                   src={src}
-//                   alt={`Image ${index + 1}`}
-//                   layout="fill"
-//                   objectFit="cover"
-//                   className="w-full h-full"
-//                 />
-//               </div>
-//             ))}
-//           </div>
-
-//           {/* Information Slider Section */}
-//           <div className="flex flex-grow w-full h-full overflow-hidden bg-[#DBE1D3] relative">
-//             {infoSections.map((info, index) => (
-//               <div
-//                 key={index}
-//                 className={`absolute inset-0 flex flex-col justify-center p-4 transition-all duration-500 ease-in-out transform ${
-//                   index === currentIndex
-//                     ? "opacity-100 translate-y-0 z-10"
-//                     : "opacity-0 translate-y-full z-0"
-//                 }`}
-//               >
-//                 <div className="px-10">
-//                   <p className="text-primaryrose text-2xl text-md">{info.title}</p>
-//                   <p className="text-6xl">{info.subtitle}</p>
-//                   <p className="text-xl">{info.description}</p>
-//                   <div className="pb-6">
-//                     <button className="bg-primaryrose hover:bg-primaryrosedark py-2 text-white px-14">
-//                       Explore
-//                     </button>
-//                   </div>
-//                 </div>
-//               </div>
-//             ))}
-//           </div>
-
-//           {/* Dot Navigation */}
-//           <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex flex-col space-y-2 z-10">
-//             {images.map((_, index) => (
-//               <button
-//                 key={index}
-//                 className={`w-5 h-5 rounded-full border-2 border-black transition-opacity duration-500 ease-in-out ${
-//                   currentIndex === index ? "bg-black opacity-100" : "bg-transparent opacity-50"
-//                 }`}
-//                 onClick={() => handleDotClick(index)}
-//               />
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default VerticalCard;
-
 
 // "use client";
 // import Image from "next/image";
 // import React, { useState, useEffect, useRef } from "react";
 
 // const images = [
-//   "/assets/tableLinen/image6.png",
-//   "/assets/tableLinen/image8.png",
-//   "/assets/tableLinen/image6.png",
-//   "/assets/tableLinen/image8.png",
+//   "/assets/shop/bathLinen.png",
+//   "/assets/shop/Group 6.png",
+//   "/assets/shop/Group 7.png",
+//   "/assets/shop/cushionsandthrows.png",
 // ];
 
 // const infoSections = [
@@ -155,6 +39,7 @@
 
 // const VerticalCard = () => {
 //   const [currentIndex, setCurrentIndex] = useState(0);
+//   const [isImageLeft, setIsImageLeft] = useState(true); // New state to track image position
 //   const cardRef = useRef<HTMLDivElement>(null);
 //   const isInView = useRef(false);
 //   const scrollTimeout = useRef<NodeJS.Timeout | null>(null);
@@ -192,6 +77,7 @@
 //         const newIndex = currentIndex + (deltaY > 0 ? 1 : -1);
 //         if (newIndex >= 0 && newIndex < images.length) {
 //           setCurrentIndex(newIndex);
+//           setIsImageLeft((prev) => !prev); // Toggle the image position
 //         }
 //       }, 100); // Adding a timeout to ensure smooth scrolling behavior
 //     };
@@ -209,52 +95,110 @@
 //   return (
 //     <div ref={cardRef} className="py-2 relative h-[100vh] w-full">
 //       <div className="flex h-full w-full bg-white shadow-lg overflow-hidden">
-//         {/* Image Slider Section */}
-//         <div className="flex-none w-[50%] h-full overflow-hidden relative">
-//           {images.map((src, index) => (
-//             <div
-//               key={index}
-//               className={`absolute inset-0 transition-all duration-500 ease-in-out transform ${
-//                 index === currentIndex
-//                   ? "opacity-100 translate-y-0 z-10"
-//                   : "opacity-0 -translate-y-full z-0"
-//               }`}
-//             >
-//               <Image
-//                 src={src}
-//                 alt={`Image ${index + 1}`}
-//                 layout="fill"
-//                 objectFit="cover"
-//                 className="w-full h-full"
-//               />
-//             </div>
-//           ))}
-//         </div>
-
-//         {/* Information Slider Section */}
-//         <div className="flex flex-grow w-full h-full overflow-hidden bg-[#DBE1D3] relative">
-//           {infoSections.map((info, index) => (
-//             <div
-//               key={index}
-//               className={`absolute inset-0 flex flex-col justify-center p-4 transition-all duration-500 ease-in-out transform ${
-//                 index === currentIndex
-//                   ? "opacity-100 translate-y-0 z-10"
-//                   : "opacity-0 translate-y-full z-0"
-//               }`}
-//             >
-//               <div className="px-10">
-//                 <p className="text-primaryrose text-2xl text-md">{info.title}</p>
-//                 <p className="text-6xl">{info.subtitle}</p>
-//                 <p className="text-xl">{info.description}</p>
-//                 <div className="pb-6">
-//                   <button className="bg-primaryrose hover:bg-primaryrosedark py-2 text-white px-14">
-//                     Explore
-//                   </button>
+//         {/* Conditional rendering based on isImageLeft */}
+//         {isImageLeft ? (
+//           <>
+//             {/* Image Slider Section */}
+//             <div className="flex-none w-[50%] h-full overflow-hidden relative">
+//               {images.map((src, index) => (
+//                 <div
+//                   key={index}
+//                   className={`absolute inset-0 transition-all duration-500 ease-in-out transform ${
+//                     index === currentIndex
+//                       ? "opacity-100 translate-y-0 z-10"
+//                       : "opacity-0 -translate-y-full z-0"
+//                   }`}
+//                 >
+//                   <Image
+//                     src={src}
+//                     alt={`Image ${index + 1}`}
+//                     layout="fill"
+//                     objectFit="cover"
+//                     className="w-full h-full"
+//                   />
 //                 </div>
-//               </div>
+//               ))}
 //             </div>
-//           ))}
-//         </div>
+
+//             {/* Information Slider Section */}
+//             <div className="flex flex-grow w-full h-full overflow-hidden bg-[#DBE1D3] relative">
+//               {infoSections.map((info, index) => (
+//                 <div
+//                   key={index}
+//                   className={`absolute inset-0 flex flex-col justify-center p-4 transition-all duration-500 ease-in-out transform ${
+//                     index === currentIndex
+//                       ? "opacity-100 translate-y-0 z-10"
+//                       : "opacity-0 translate-y-full z-0"
+//                   }`}
+//                 >
+//                   <div className="px-10">
+//                     <p className="text-primaryrose text-2xl text-md">
+//                       {info.title}
+//                     </p>
+//                     <p className="text-6xl">{info.subtitle}</p>
+//                     <p className="text-xl">{info.description}</p>
+//                     <div className="pb-6">
+//                       <button className="bg-primaryrose hover:bg-primaryrosedark py-2 text-white px-14">
+//                         Explore
+//                       </button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               ))}
+//             </div>
+//           </>
+//         ) : (
+//           <>
+//             {/* Information Slider Section */}
+//             <div className="flex flex-grow w-full h-full overflow-hidden bg-[#DBE1D3] relative">
+//               {infoSections.map((info, index) => (
+//                 <div
+//                   key={index}
+//                   className={`absolute inset-0 flex flex-col justify-center p-4 transition-all duration-500 ease-in-out transform ${
+//                     index === currentIndex
+//                       ? "opacity-100 translate-y-0 z-10"
+//                       : "opacity-0 translate-y-full z-0"
+//                   }`}
+//                 >
+//                   <div className="px-10">
+//                     <p className="text-primaryrose text-2xl text-md">
+//                       {info.title}
+//                     </p>
+//                     <p className="text-6xl">{info.subtitle}</p>
+//                     <p className="text-xl">{info.description}</p>
+//                     <div className="pb-6">
+//                       <button className="bg-primaryrose hover:bg-primaryrosedark py-2 text-white px-14">
+//                         Explore
+//                       </button>
+//                     </div>
+//                   </div>
+//                 </div>
+//               ))}
+//             </div>
+
+//             {/* Image Slider Section */}
+//             <div className="flex-none w-[50%] h-full overflow-hidden relative">
+//               {images.map((src, index) => (
+//                 <div
+//                   key={index}
+//                   className={`absolute inset-0 transition-all duration-500 ease-in-out transform ${
+//                     index === currentIndex
+//                       ? "opacity-100 translate-y-0 z-10"
+//                       : "opacity-0 -translate-y-full z-0"
+//                   }`}
+//                 >
+//                   <Image
+//                     src={src}
+//                     alt={`Image ${index + 1}`}
+//                     layout="fill"
+//                     objectFit="cover"
+//                     className="w-full h-full"
+//                   />
+//                 </div>
+//               ))}
+//             </div>
+//           </>
+//         )}
 
 //         {/* Dot Navigation */}
 //         <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex flex-col space-y-2 z-10">
@@ -266,7 +210,7 @@
 //                   ? "bg-black opacity-100"
 //                   : "bg-transparent opacity-50"
 //               }`}
-//               disabled // Disable the click
+             
 //             />
 //           ))}
 //         </div>
@@ -276,15 +220,17 @@
 // };
 
 // export default VerticalCard;
+
+
 "use client";
 import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
 
 const images = [
-  "/assets/tableLinen/image6.png",
-  "/assets/tableLinen/image8.png",
-  "/assets/tableLinen/image6.png",
-  "/assets/tableLinen/image8.png",
+  "/assets/shop/bathLinen.png",
+  "/assets/shop/Group 6.png",
+  "/assets/shop/Group 7.png",
+  "/assets/shop/cushionsandthrows.png",
 ];
 
 const infoSections = [
@@ -316,7 +262,7 @@ const infoSections = [
 
 const VerticalCard = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isImageLeft, setIsImageLeft] = useState(true); // New state to track image position
+  const [isImageLeft, setIsImageLeft] = useState(true); // Track image position
   const cardRef = useRef<HTMLDivElement>(null);
   const isInView = useRef(false);
   const scrollTimeout = useRef<NodeJS.Timeout | null>(null);
@@ -370,13 +316,13 @@ const VerticalCard = () => {
   }, [currentIndex]);
 
   return (
-    <div ref={cardRef} className="py-2 relative h-[100vh] w-full">
-      <div className="flex h-full w-full bg-white shadow-lg overflow-hidden">
+    <div ref={cardRef} className="py-2 relative h-[100vh] w-full overflow-hidden">
+      <div className="flex h-full w-full bg-white shadow-lg">
         {/* Conditional rendering based on isImageLeft */}
         {isImageLeft ? (
           <>
             {/* Image Slider Section */}
-            <div className="flex-none w-[50%] h-full overflow-hidden relative">
+            <div className="flex-none w-[40%] h-[80%] overflow-hidden relative">
               {images.map((src, index) => (
                 <div
                   key={index}
@@ -390,7 +336,7 @@ const VerticalCard = () => {
                     src={src}
                     alt={`Image ${index + 1}`}
                     layout="fill"
-                    objectFit="cover"
+                    objectFit="contain"
                     className="w-full h-full"
                   />
                 </div>
@@ -409,10 +355,8 @@ const VerticalCard = () => {
                   }`}
                 >
                   <div className="px-10">
-                    <p className="text-primaryrose text-2xl text-md">
-                      {info.title}
-                    </p>
-                    <p className="text-6xl">{info.subtitle}</p>
+                    <p className="text-primaryrose text-2xl">{info.title}</p>
+                    <p className="text-4xl">{info.subtitle}</p>
                     <p className="text-xl">{info.description}</p>
                     <div className="pb-6">
                       <button className="bg-primaryrose hover:bg-primaryrosedark py-2 text-white px-14">
@@ -438,10 +382,8 @@ const VerticalCard = () => {
                   }`}
                 >
                   <div className="px-10">
-                    <p className="text-primaryrose text-2xl text-md">
-                      {info.title}
-                    </p>
-                    <p className="text-6xl">{info.subtitle}</p>
+                    <p className="text-primaryrose text-2xl">{info.title}</p>
+                    <p className="text-4xl">{info.subtitle}</p>
                     <p className="text-xl">{info.description}</p>
                     <div className="pb-6">
                       <button className="bg-primaryrose hover:bg-primaryrosedark py-2 text-white px-14">
@@ -454,7 +396,7 @@ const VerticalCard = () => {
             </div>
 
             {/* Image Slider Section */}
-            <div className="flex-none w-[50%] h-full overflow-hidden relative">
+            <div className="flex-none w-[40%] h-[80%] overflow-hidden relative">
               {images.map((src, index) => (
                 <div
                   key={index}
@@ -468,7 +410,7 @@ const VerticalCard = () => {
                     src={src}
                     alt={`Image ${index + 1}`}
                     layout="fill"
-                    objectFit="cover"
+                    objectFit="contain"
                     className="w-full h-full"
                   />
                 </div>
@@ -479,18 +421,19 @@ const VerticalCard = () => {
 
         {/* Dot Navigation */}
         <div className="absolute right-2 top-1/2 transform -translate-y-1/2 flex flex-col space-y-2 z-10">
-          {images.map((_, index) => (
-            <button
-              key={index}
-              className={`w-5 h-5 rounded-full border-2 border-black transition-opacity duration-500 ease-in-out ${
-                currentIndex === index
-                  ? "bg-black opacity-100"
-                  : "bg-transparent opacity-50"
-              }`}
-             
-            />
-          ))}
-        </div>
+  {images.map((_, index) => (
+    <button
+      key={index}
+      onClick={() => setCurrentIndex(index)} // Update currentIndex on click
+      className={`w-5 h-5 rounded-full border-2 border-black transition-opacity duration-500 ease-in-out ${
+        currentIndex === index
+          ? "bg-primaryrosedark opacity-100"
+          : "bg-transparent opacity-50"
+      }`}
+    />
+  ))}
+</div>
+
       </div>
     </div>
   );
--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,101 +1,77 @@
-import React from "react";
+// import React from "react";
+// import HeaderLayout from "../components/HeaderLayout";
+// import Footer from "../components/DashComponents/Footer";
+
+// const DesignPage = () => {
+//   return (
+//     <>
+//       <HeaderLayout OtherPage={undefined}>.</HeaderLayout>
+//       <div className="flex text-6xl bg-primaryrose items-center justify-center">
+//         Design
+//       </div>
+
+      
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default DesignPage;
+
+
+import Image from 'next/image'
+
 import HeaderLayout from "../components/HeaderLayout";
-import Footer from "../components/DashComponents/Footer";
+import Footer from '../components/DashComponents/Footer';
 
-const DesignPage = () => {
+export default function DesignPage() {
   return (
-    <>
+    <div>
       <HeaderLayout OtherPage={undefined}>.</HeaderLayout>
-      <div className="flex text-6xl bg-primaryrose items-center justify-center">
-        Design
-      </div>
-
-      {/* <div>
-        <div
-          className="pb-4"
-          style={{
-            backgroundImage: `url('/assets/dashboard/master18.jpeg')`,
-          }}
-        >
-          <div className="grid grid-cols-4 gap-4">
-            <div className="col-span-2 row-span-1">
-              <Image
-                src="/assets/dashboard/shop1.jpeg"
-                alt="First Image"
-                className="h-[60vh] w-full object-cover"
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div className="col-span-1 row-span-1">
-              <Image
-                src="/assets/dashboard/shop2.jpeg"
-                alt="Second Image"
-                className="h-[40vh] w-full object-cover"
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div className="col-span-1 row-span-1">
-              <Image
-                src="/assets/dashboard/shop3.jpeg"
-                alt="Third Image"
-                className="h-[40vh] w-full object-cover"
-                width={1000}
-                height={1000}
-              />
-            </div>
-
-            <div className="col-span-1 row-span-1">
-              <Image
-                src="/assets/dashboard/shop4.jpeg"
-                alt="Fourth Image"
-                className="h-[40vh] w-full object-cover "
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div className="col-span-1 row-span-1 ">
-              <Image
-                src="/assets/dashboard/shop5.jpeg"
-                alt="Fifth Image"
-                className="h-[40vh] w-full object-cover "
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div className="col-span-1 row-span-1 mt-[-11rem]">
-              <Image
-                src="/assets/dashboard/shop6.jpeg"
-                alt="Sixth Image"
-                className="h-[60vh] w-full object-cover"
-                width={1000}
-                height={1000}
-              />
-            </div>
-            <div className="col-span-1 row-span-1 mt-[-11rem]">
-              <Image
-                src="/assets/dashboard/shop5.jpeg"
-                alt="Fifth Image"
-                className="h-[30vh] w-full object-cover pb-6"
-                width={1000}
-                height={1000}
-              />
-              <Image
-                src="/assets/dashboard/shop5.jpeg"
-                alt="Fifth Image"
-                className="h-[30vh] w-full object-cover "
-                width={1000}
-                height={1000}
-              />
-            </div>
-           
-          </div>
+      
+      
+       <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-5xl w-full">
+        {/* Carousel Section */}
+        <div className="relative w-full h-96">
+          <Image 
+            src="/assets/design/design.jpg" 
+            alt="Bedroom Design" 
+            layout="fill" 
+            objectFit="cover" 
+            className="w-full h-full object-cover"
+          />
+          {/* Plus (+) Interactive Icons */}
+          <div className="absolute top-1/3 left-1/4 bg-white p-2 rounded-full shadow-lg cursor-pointer">+</div>
+          <div className="absolute top-1/2 left-1/3 bg-white p-2 rounded-full shadow-lg cursor-pointer">+</div>
+          <div className="absolute top-2/3 left-1/2 bg-white p-2 rounded-full shadow-lg cursor-pointer">+</div>
+          <div className="absolute top-1/2 right-1/4 bg-white p-2 rounded-full shadow-lg cursor-pointer">+</div>
         </div>
-      </div> */}
-      <Footer />
-    </>
-  );
-};
+        {/* Product Info Section */}
+        <div className="flex justify-between p-6">
+          <div className="text-left">
+            <h2 className="text-lg font-semibold">Beehive Glass Vase Olive</h2>
+            <p className="text-gray-500">
+              <span className="line-through">₹5,490.00</span> 
+              <span className="text-red-500 ml-2">₹3,897.90</span>
+            </p>
+          </div>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+            View Product
+          </button>
+        </div>
+        {/* Carousel Pagination */}
+        <div className="flex justify-center space-x-2 p-4">
+          <div className="h-2 w-2 bg-gray-500 rounded-full"></div>
+          <div className="h-2 w-2 bg-gray-300 rounded-full"></div>
+          <div className="h-2 w-2 bg-gray-300 rounded-full"></div>
+          <div className="h-2 w-2 bg-gray-300 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+    <Footer/>
 
-export default DesignPage;
+    </div>
+
+  )
+}
